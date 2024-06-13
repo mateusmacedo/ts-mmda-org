a2876e36622b709bf278e6d71dc3742df4009d32
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-13 07:23:19 -0300

    test(config): :test_tube: enable coverage collection in jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,10 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts'
+  ],
+  coverageDirectory: '<rootDir>/coverage',
 });
