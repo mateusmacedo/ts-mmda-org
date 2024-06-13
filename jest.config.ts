28bb96c8a1371245bb57643308b5e88e782b9ec0
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-13 07:26:52 -0300

    style(config): :gem: reformat config files for readability

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,6 @@ import { getJestProjectsAsync } from '@nx/jest';
 export default async () => ({
   projects: await getJestProjectsAsync(),
   collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/**/*.d.ts'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
   coverageDirectory: '<rootDir>/coverage',
 });
