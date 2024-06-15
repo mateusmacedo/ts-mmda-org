f4dbff748fccdf701d381eadc91676befa8a1c12
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-14 22:06:44 -0300

    ci(config): :construction_worker: add --uncommitted flag to lint-staged tasks
    Optimizes `pre-commit` pipeline by only running tasks on uncommitted files, speeding up the workflow and reducing unnecessary checks.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '{libs,apps}/**/*.{js,ts,tsx}': [
-    'nx affected --target test',
-    'nx affected --target lint --fix=true',
-    'nx format:write',
+    'nx affected --target lint --fix=true --uncommitted=true',
+    'nx format:write --uncommitted=true',
+    'nx affected --target test --uncommitted=true',
   ],
 };
