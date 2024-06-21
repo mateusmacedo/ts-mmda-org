722066c8574ebecbe96b616b2ac72d58728d438f
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 18:11:54 -0300

    ci(lint-staged): :construction_worker: allow tests to pass with no tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
   '{libs,apps}/**/*.{js,ts,tsx}': [
     'nx format:write --uncommitted=true',
     'nx affected --target lint --fix=true --uncommitted=true --parallel=5',
-    'nx affected --target test --uncommitted=true --parallel=5',
+    'nx affected --target test --uncommitted=true --parallel=5 --passWithNoTests=true',
   ],
 };
