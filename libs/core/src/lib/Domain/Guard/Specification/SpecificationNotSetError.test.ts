2dbe82996a25c8632e747bf6d2b8905e52f50d85
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 18:12:22 -0300

    test(domain): :test_tube: add tests for SpecificationNotSetError
    Added unit tests to verify correct name and message for SpecificationNotSetError, ensuring greater reliability and easier maintenance.

---
+++ libs/core/src/lib/Domain/Guard/Specification/SpecificationNotSetError.test.ts
@@ -0,0 +1,13 @@
+import { SpecificationNotSetError } from './SpecificationNotSetError';
+
+describe('SpecificationNotSetError', () => {
+  it('should have the correct name', () => {
+    const error = new SpecificationNotSetError();
+    expect(error.name).toEqual('SpecificationNotSetError');
+  });
+
+  it('should have the correct message', () => {
+    const error = new SpecificationNotSetError();
+    expect(error.message).toEqual('Initial specification is not set.');
+  });
+});
