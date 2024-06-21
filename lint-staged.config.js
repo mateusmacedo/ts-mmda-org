d1dc3326a5d80bc0925dc29ef3fe0d43c6d0c7a4
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 18:05:30 -0300

    ci(config): :construction_worker: enable parallel execution for faster builds

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '{libs,apps}/**/*.{js,ts,tsx}': [
-    'nx affected --target lint --fix=true --uncommitted=true',
     'nx format:write --uncommitted=true',
-    'nx affected --target test --uncommitted=true',
+    'nx affected --target lint --fix=true --uncommitted=true --parallel=5',
+    'nx affected --target test --uncommitted=true --parallel=5',
   ],
 };
