7a0ce27337a52239cdf29e7f7b4f870110d406fc
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 17:26:59 -0300

    refactor(core): :hammer: simplify specifications by removing redundant methods

--- libs/core/src/lib/Domain/Guard/Specification/ISpecification.ts
+++ libs/core/src/lib/Domain/Guard/Specification/ISpecification.ts
@@ -2,7 +2,4 @@ export type Candidate<T> = T | T[];
 
 export interface ISpecification<T> {
   isSatisfiedBy(candidate: Candidate<T>): boolean;
-  and(...specs: ISpecification<T>[]): ISpecification<T>;
-  or(...specs: ISpecification<T>[]): ISpecification<T>;
-  not(): ISpecification<T>;
 }
