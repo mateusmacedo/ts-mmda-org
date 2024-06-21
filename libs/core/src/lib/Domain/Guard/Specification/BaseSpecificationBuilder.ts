371429af6249a26f45289f08b2fdeb27fd0b9576
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 18:13:00 -0300

    feat(core): :sparkles: add base specification builder and interface for domain guard

---
+++ libs/core/src/lib/Domain/Guard/Specification/BaseSpecificationBuilder.ts
@@ -0,0 +1,44 @@
+import { ISpecification } from './ISpecification';
+import { SpecificationBuilder } from './ISpecificationBuilder';
+import { SpecificationNotSetError } from './SpecificationNotSetError';
+import { AndSpecification, NotSpecification, OrSpecification } from './Specifications';
+
+export class BaseSpecificationBuilder<T> implements SpecificationBuilder<T> {
+  private spec: ISpecification<T> | null = null;
+
+  withSpecification(spec: ISpecification<T>): SpecificationBuilder<T> {
+    this.spec = spec;
+    return this;
+  }
+
+  and(...specs: ISpecification<T>[]): SpecificationBuilder<T> {
+    if (this.spec === null) {
+      throw new SpecificationNotSetError();
+    }
+    this.spec = new AndSpecification<T>(this.spec, ...specs);
+    return this;
+  }
+
+  or(...specs: ISpecification<T>[]): SpecificationBuilder<T> {
+    if (this.spec === null) {
+      throw new SpecificationNotSetError();
+    }
+    this.spec = new OrSpecification<T>(this.spec, ...specs);
+    return this;
+  }
+
+  not(): SpecificationBuilder<T> {
+    if (this.spec === null) {
+      throw new SpecificationNotSetError();
+    }
+    this.spec = new NotSpecification<T>(this.spec);
+    return this;
+  }
+
+  build(): ISpecification<T> {
+    if (this.spec === null) {
+      throw new SpecificationNotSetError();
+    }
+    return this.spec;
+  }
+}
