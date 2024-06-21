53a58f19da841db69b159f89d19fa0c45b70dbe2
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-21 20:01:09 -0300

    feat(domain): :sparkles: introduce instance factory
    Add BasicFactory for creating instances with error handling.

---
+++ libs/core/src/lib/Domain/Factory/InstanceFactory.ts
@@ -0,0 +1,29 @@
+import { InstanceCreationError, InvalidPropsError, TargetConstructorError } from './Errors';
+
+export interface Factory {
+  create<T>(target: new (...args: unknown[]) => T, props?: ConstructorParameters<typeof target>): T;
+}
+
+export class BasicFactory implements Factory {
+  create<T>(
+    target: new (...args: unknown[]) => T,
+    props?: ConstructorParameters<typeof target>,
+  ): T {
+    if (!target) {
+      throw new TargetConstructorError('Target constructor is required');
+    }
+
+    if (props && !Array.isArray(props)) {
+      throw new InvalidPropsError('Props must be an array if provided');
+    }
+
+    try {
+      const instance = new target(...(props ?? []));
+      return instance;
+    } catch (error) {
+      throw new InstanceCreationError(
+        `Could not create instance of ${target.name}: ${error.message}`,
+      );
+    }
+  }
+}
