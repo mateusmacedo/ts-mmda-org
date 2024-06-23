5d8cde9f6c4d7b89a4de8ed4775cf29950e571f2
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-23 14:56:41 -0300

    fix(repository): :bug: add error handling for duplicate and missing entities

--- libs/core/src/lib/Infrastructure/InMemoryRepository.ts
+++ libs/core/src/lib/Infrastructure/InMemoryRepository.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, BaseEntityProps, Repository } from '../Domain';
+import { BaseEntity, BaseEntityProps, Repository, RepositoryError } from '../Domain';
 
 export class InMemoryRepository<T extends BaseEntity<BaseEntityProps<ID>, ID>, ID>
   implements Repository<T, ID>
@@ -18,15 +18,24 @@ export class InMemoryRepository<T extends BaseEntity<BaseEntityProps<ID>, ID>, I
   }
 
   async save(entity: T): Promise<T> {
+    if (this.entities.has(entity.getId().toString())) {
+      throw new RepositoryError(`Entity with ID ${entity.getId().toString()} already exists.`);
+    }
     this.entities.set(entity.getId().toString(), entity);
     return entity;
   }
 
   async delete(entity: T): Promise<void> {
+    if (!this.entities.has(entity.getId().toString())) {
+      throw new RepositoryError(`Entity with ID ${entity.getId().toString()} not found.`);
+    }
     this.entities.delete(entity.getId().toString());
   }
 
   async deleteById(id: ID): Promise<void> {
+    if (!this.entities.has(id.toString())) {
+      throw new RepositoryError(`Entity with ID ${id.toString()} not found.`);
+    }
     this.entities.delete(id.toString());
   }
 }
