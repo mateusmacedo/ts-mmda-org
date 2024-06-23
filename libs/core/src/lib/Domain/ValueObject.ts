194ada8a1be53aff7e738151ae450d324e9c455f
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-23 12:41:48 -0300

    refactor(core): :hammer: ensure immutability and enhance prop comparison

--- libs/core/src/lib/Domain/ValueObject.ts
+++ libs/core/src/lib/Domain/ValueObject.ts
@@ -9,33 +9,45 @@ export interface ValueObject<T extends ValueObjectProps<T>> {
 }
 
 export abstract class BaseValueObject<T extends ValueObjectProps<T>> implements ValueObject<T> {
-  constructor(protected props: T) {
-    this.props = props ?? ({} as T);
+  protected readonly props: T;
+
+  constructor(props?: T) {
+    this.props = Object.freeze(props || ({} as T)); // Ensure immutability and handle undefined props
   }
 
   public equals(vo: ValueObject<T>): boolean {
     if (vo.constructor.name !== this.constructor.name) {
       return false;
     }
-
-    const otherProps = vo.toValue();
-    return this.compareProps(this.props, otherProps);
+    return this.compareProps(this.props, vo.toValue());
   }
 
   private compareProps(props1: ValueObjectProps<T>, props2: ValueObjectProps<T>): boolean {
-    const keys1 = Object.keys(props1);
-    const keys2 = Object.keys(props2);
+    const keys1 = Object.keys(props1) as Array<keyof T>;
+    const keys2 = Object.keys(props2) as Array<keyof T>;
 
     if (keys1.length !== keys2.length) {
       return false;
     }
 
     for (const key of keys1) {
-      if (!Object.prototype.hasOwnProperty.call(props2, key) || props1[key] !== props2[key]) {
+      const val1 = props1[key];
+      const val2 = props2[key];
+
+      const areObjects = this.isObject(val1) && this.isObject(val2);
+      if (
+        (areObjects &&
+          !this.compareProps(val1 as ValueObjectProps<T>, val2 as ValueObjectProps<T>)) ||
+        (!areObjects && val1 !== val2)
+      ) {
         return false;
       }
     }
     return true;
+  }
+
+  private isObject(obj: unknown): boolean {
+    return obj !== null && typeof obj === 'object';
   }
 
   public toString(): string {
