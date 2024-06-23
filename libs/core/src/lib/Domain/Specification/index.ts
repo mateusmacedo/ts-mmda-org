74738a6677cf18047f8f0b510555ea917d4969bf
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-23 03:40:43 -0300

    feat(domain): :sparkles: add index files for better module imports
    Added index files to the Specification, Validation, and Domain modules for simplifying and improving the import structure across the codebase.

---
+++ libs/core/src/lib/Domain/Specification/index.ts
@@ -0,0 +1,5 @@
+export * from './BaseSpecificationBuilder';
+export * from './ISpecification';
+export * from './ISpecificationBuilder';
+export * from './SpecificationErrors';
+export * from './Specifications';
