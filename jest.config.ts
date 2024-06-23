d2e7f2496c944524beb316a5ea36cc9ea0560332
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-23 07:12:08 -0300

    test(config): :test_tube: exclude index.ts from coverage collection

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,11 @@ import { getJestProjectsAsync } from '@nx/jest';
 export default async () => ({
   projects: await getJestProjectsAsync(),
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/index.ts',
+  ],
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
 });
