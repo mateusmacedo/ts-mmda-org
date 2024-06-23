03dc0c956ffb281ca65b5870e714d6969c998e50
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-23 09:23:55 -0300

    test(core): :test_tube: improve message bus coverage and clarity

--- libs/core/src/lib/Application/MessageBus.test.ts
+++ libs/core/src/lib/Application/MessageBus.test.ts
@@ -2,7 +2,6 @@ import { EMessageStatus, TMessageProps } from '../Domain/Messages';
 import { BasicMessageBus } from './MessageBus';
 import { Command, Event, IQueryHandler, Query, TCommandHandler, TEventHandler } from './Messages';
 
-// Mock classes for testing
 class TestCommand extends Command<unknown> {
   constructor(payload: unknown) {
     const props: TMessageProps<unknown, unknown> = {
@@ -76,15 +75,24 @@ describe('BasicMessageBus', () => {
     messageBus = new BasicMessageBus();
   });
 
-  // Happy Path Tests
-  it('should register and handle a command', async () => {
+  // Testes de Caminho Feliz
+  it('should register and treat a command', async () => {
     const handler = new TestCommandHandler();
     messageBus.registerCommandHandler('TestCommand', handler);
     const command = new TestCommand({ some: 'commandPayload' });
     await expect(messageBus.sendCommand(command)).resolves.not.toThrow();
   });
 
-  it('should register and handle a query', async () => {
+  it('should register and treat multiple command handlers', async () => {
+    const handler1 = new TestCommandHandler();
+    const handler2 = new TestCommandHandler();
+    messageBus.registerCommandHandler('TestCommand', handler1);
+    messageBus.registerCommandHandler('TestCommand', handler2);
+    const command = new TestCommand({ some: 'commandPayload' });
+    await expect(messageBus.sendCommand(command)).resolves.not.toThrow();
+  });
+
+  it('should register and treat an appointment', async () => {
     const handler = new TestQueryHandler();
     messageBus.registerQueryHandler('TestQuery', handler);
     const query = new TestQuery({ some: 'queryPayload' });
@@ -93,45 +101,20 @@ describe('BasicMessageBus', () => {
     );
   });
 
-  it('should register and handle an event', async () => {
+  it('should register and treat multiple query handlers', async () => {
+    const handler1 = new TestQueryHandler();
+    const handler2 = new DifferentTestQueryHandler();
+    messageBus.registerQueryHandler('TestQuery', handler1);
+    expect(() => messageBus.registerQueryHandler('TestQuery', handler2)).toThrow(
+      `A handler is already registered for query type TestQuery`,
+    );
+  });
+
+  it('should register and treat an event', async () => {
     const handler = new TestEventHandler();
     messageBus.registerEventHandler('TestEvent', handler);
     const event = new TestEvent({ some: 'eventPayload' });
     await expect(messageBus.publishEvent(event)).resolves.not.toThrow();
-  });
-
-  // Edge Case Tests
-  it('should overwrite a command handler with an existing command type', async () => {
-    const handler1 = new TestCommandHandler();
-    const handler2 = new TestCommandHandler();
-    messageBus.registerCommandHandler('TestCommand', handler1);
-    messageBus.registerCommandHandler('TestCommand', handler2); // Overwrite
-    const command = new TestCommand({ some: 'commandPayload' });
-    await expect(messageBus.sendCommand(command)).resolves.not.toThrow();
-  });
-
-  it('should overwrite a query handler with an existing query type', async () => {
-    const handler1 = new TestQueryHandler();
-    const handler2 = new DifferentTestQueryHandler();
-    messageBus.registerQueryHandler('TestQuery', handler1);
-    messageBus.registerQueryHandler('TestQuery', handler2); // Overwrite
-    const query = new TestQuery({ some: 'queryPayload' });
-    await expect(messageBus.sendQuery(query)).resolves.toBe(
-      'Different handler response with payload: {"some":"queryPayload"}',
-    );
-  });
-
-  it('should add event handler to existing handlers for the same event type', async () => {
-    const handler1 = new TestEventHandler();
-    const handler2 = new TestEventHandler();
-    jest.spyOn(handler1, 'handle');
-    jest.spyOn(handler2, 'handle');
-    messageBus.registerEventHandler('TestEvent', handler1);
-    messageBus.registerEventHandler('TestEvent', handler2); // Add to existing
-    const event = new TestEvent({ some: 'eventPayload' });
-    await messageBus.publishEvent(event);
-    expect(handler1.handle).toHaveBeenCalled();
-    expect(handler2.handle).toHaveBeenCalled();
   });
 
   it('should throw an error if no handler is registered for a command', async () => {
@@ -148,13 +131,12 @@ describe('BasicMessageBus', () => {
     );
   });
 
-  it('should not throw an error if no handlers are registered for an event', async () => {
+  it('should not release an error if no handler is registered for an event', async () => {
     const event = new TestEvent({ some: 'eventPayload' });
     await expect(messageBus.publishEvent(event)).resolves.not.toThrow();
   });
 
-  // Middleware Tests
-  it('should apply middlewares for a command', async () => {
+  it('should apply middlewares to a command', async () => {
     const handler = new TestCommandHandler();
     messageBus.registerCommandHandler('TestCommand', handler);
 
@@ -167,7 +149,7 @@ describe('BasicMessageBus', () => {
     expect(middleware).toHaveBeenCalledWith(command, expect.any(Function));
   });
 
-  it('should apply middlewares for a query', async () => {
+  it('should apply middlewares to a query', async () => {
     const handler = new TestQueryHandler();
     messageBus.registerQueryHandler('TestQuery', handler);
 
@@ -180,7 +162,7 @@ describe('BasicMessageBus', () => {
     expect(middleware).toHaveBeenCalledWith(query, expect.any(Function));
   });
 
-  it('should apply middlewares for an event', async () => {
+  it('should apply middlewares to an event', async () => {
     const handler = new TestEventHandler();
     messageBus.registerEventHandler('TestEvent', handler);
 
@@ -193,7 +175,7 @@ describe('BasicMessageBus', () => {
     expect(middleware).toHaveBeenCalledWith(event, expect.any(Function));
   });
 
-  it('should call next middleware in chain', async () => {
+  it('should call the next middleware in the chain', async () => {
     const handler = new TestCommandHandler();
     messageBus.registerCommandHandler('TestCommand', handler);
 
@@ -211,5 +193,39 @@ describe('BasicMessageBus', () => {
     expect(middleware1).toHaveBeenCalledWith(command, expect.any(Function));
     expect(middleware2).toHaveBeenCalledWith(command, expect.any(Function));
     expect(middleware3).toHaveBeenCalledWith(command, expect.any(Function));
+  });
+
+  it('should continue to run all handlers even if one fails for a command', async () => {
+    const handler1 = {
+      handle: jest.fn().mockRejectedValue(new Error('Handler 1 failed')),
+    };
+    const handler2 = {
+      handle: jest.fn().mockResolvedValue(undefined),
+    };
+    messageBus.registerCommandHandler('TestCommand', handler1);
+    messageBus.registerCommandHandler('TestCommand', handler2);
+
+    const command = new TestCommand({ some: 'commandPayload' });
+    await expect(messageBus.sendCommand(command)).resolves.not.toThrow();
+
+    expect(handler1.handle).toHaveBeenCalled();
+    expect(handler2.handle).toHaveBeenCalled();
+  });
+
+  it('should continue to run all handlers even if one fails for an event', async () => {
+    const handler1 = {
+      handle: jest.fn().mockRejectedValue(new Error('Handler 1 failed')),
+    };
+    const handler2 = {
+      handle: jest.fn().mockResolvedValue(undefined),
+    };
+    messageBus.registerEventHandler('TestEvent', handler1);
+    messageBus.registerEventHandler('TestEvent', handler2);
+
+    const event = new TestEvent({ some: 'eventPayload' });
+    await expect(messageBus.publishEvent(event)).resolves.not.toThrow();
+
+    expect(handler1.handle).toHaveBeenCalled();
+    expect(handler2.handle).toHaveBeenCalled();
   });
 });
