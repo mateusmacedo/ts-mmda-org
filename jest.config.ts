60b1590440d42422f0183db549ddfa68d1d61adf
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-26 08:23:47 -0300

    chore(test): :wrench: remove redundant coverage settings
    Removed unnecessary coverage collection configurations from jest.config.ts to streamline test setup.

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,4 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/**/*.d.ts',
-    '!<rootDir>/src/**/index.ts',
-  ],
-  coverageDirectory: '<rootDir>/coverage',
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
 });
